67f74f5a4ac60f0f0c4f934e8488afb3ba01e2b0
Julia <jul.semeniuk@gmail.com>
2024-08-06 13:04:12 +0300

    fix: mui-theme-setup fixes TS errors
    feat: mui-theme-setup creates theme
    
    feat: mui-theme-setup adds custom typography variants
    
    fix: mui-theme-setup cleans up

--- src/components/OperatorList/OperatorList.tsx
+++ src/components/OperatorList/OperatorList.tsx
@@ -1,8 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
-import { AppDispatch, RootState } from 'store';
+import { AppDispatch, RootState } from '../../store';
 import { getOperatorsRequest, getOperatorsAddonsRequest } from '../../slices/operatorSlice'; //todo: fix the abs path
+import React from 'react';
+import { TextField, Typography } from '@mui/material';
 
 const OperatorList: React.FC = () => {
     const dispatch = useDispatch<AppDispatch>();
@@ -19,20 +21,25 @@ const OperatorList: React.FC = () => {
     if (error) return <p>Error: {error}</p>;
 
     return (
-        <div>
-            <h1>Operators</h1>
-            <ul>
-                {operators.map((operator) => (
-                    <li key={operator.id}>{operator.name}</li>
-                ))}
-            </ul>
-            <h1>Operator Addons</h1>
-            <ul>
-                {operatorAddons.map((addon) => (
-                    <li key={addon.id}>{addon.text}</li>
-                ))}
-            </ul>
-        </div>
+        <>
+            <TextField label="Search" placeholder="User name..." />
+            <div>
+                <Typography variant="h1">Operators</Typography>
+                <ul>
+                    {operators.map((operator) => (
+                        <li key={operator.id}>
+                            <Typography variant="body2">{operator.name}</Typography>
+                        </li>
+                    ))}
+                </ul>
+                <h1>Operator Addons</h1>
+                <ul>
+                    {operatorAddons.map((addon) => (
+                        <li key={addon.id}>{addon.text}</li>
+                    ))}
+                </ul>
+            </div>
+        </>
     );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
+import { CssBaseline } from '@mui/material';
+import theme from './theme';
+import { ThemeProvider } from 'styled-components';
 
 const rootElement = document.getElementById('root');
 
@@ -10,7 +13,10 @@ if (rootElement) {
     ReactDOM.createRoot(rootElement).render(
         <React.StrictMode>
             <Provider store={store}>
-                <App />
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <App />
+                </ThemeProvider>
             </Provider>
         </React.StrictMode>
     );
